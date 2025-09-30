f800b861d1d4c005a10fdfaca0845d4a9838bd50
lauolivera90 <lautarobolivera098@gmail.com>
2025-09-30 14:37:45 -0300

    Update security.js

--- src/middlewares/security.js
+++ src/middlewares/security.js
@@ -21,7 +21,7 @@ const applySecurityMiddlewares = (app) => {
 
   const limiter = rateLimit({
     windowMs: 15 * 60 * 1000,
-    max: 100,
+    max: 500,
     message: {
       error: "Too many requests, please try again later."
     }
